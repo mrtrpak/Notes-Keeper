0b17f6a3e58d94039b3170ab97adb96cf89acd3a
Michael Trpak <mrtrpak3@gmail.com>
2019-11-20 21:24:46 -0500

    added dependencies

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const notes = require("./db/db.json");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
