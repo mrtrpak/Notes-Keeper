ab065cd4cceafabffc96d2fdd77a75a148df1f7b
Michael Trpak <mrtrpak3@gmail.com>
2019-11-20 21:26:35 -0500

    added in routes and api

--- server.js
+++ server.js
@@ -9,3 +9,31 @@ const PORT = process.env.PORT || 3333;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", (req, res)=> {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// convenience variable
+const source = "./db/db.json";
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile(source, (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = notes.length + 1;
+    notes.push(newNote);
+    fs.writeFile(source, JSON.stringify(notes), err => {
+        if (err) return err;
+        res.sendStatus(200);
+    });
+});
